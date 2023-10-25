6674fa07a909b5b16c5fe934e0b7f72ea5b03811
savidyainduma <Savidyainduma@gmail.com>
2023-10-25 12:34:11 +0530

    header and footer created

--- src/Components/GameBoard.js
+++ src/Components/GameBoard.js
@@ -1,7 +1,10 @@
 import React, {useState} from "react";
 import GameCircle from "./GameCircle";
 import '../Game.css';
+import Header from "./Header";
+import Footer from "./Footer";
 
+const NO_CIRCLES = 16;
 const NO_PLAYER = 0;
 const PLAYER_1 = 1;
 const PLAYER_2 = 2;
@@ -10,33 +13,45 @@ const GameBoard= (props) =>
     
        
     
-    const [gameBoard, setGameBoard] = useState(Array(16).fill(0));
+    const [gameBoard, setGameBoard] = useState(Array(16).fill(NO_PLAYER));
     const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1);
+    console.log(gameBoard);
+
+    const initBoard = () => {
+        const circles =[];
+        for(let i=0; i<NO_CIRCLES; i++)
+        {
+            circles.push(renderCircle(i));
+        }
+        return circles;
+    }
+
     const circleClicked = (id)=>{
         console.log('circle clicked: '+id);
-
-        const board = [...gameBoard];
-        board[id]= currentPlayer;
-        setGameBoard(board);
+        
+        setGameBoard(prev => {
+            return prev.map((circle, pos) =>{
+                if(pos === id) return currentPlayer;
+                return circle;
+            })
+        } )
+     
         setCurrentPlayer(currentPlayer === PLAYER_1 ? PLAYER_2 : PLAYER_1);
         console.log(gameBoard);
         console.log(currentPlayer);
     }
     const renderCircle = id => {
-        return <GameCircle id={id} className= {`player_${gameBoard[id]}`} onCircleClicked={circleClicked} />
+        return <GameCircle key={id} id={id} className= {`player_${gameBoard[id]}`} onCircleClicked={circleClicked} />
     }
 
     return (
-        <div className="gameBoard" >
-            {renderCircle(0)}
-            {renderCircle(1)}
-            {renderCircle(2)}
-            {renderCircle(3)}
-            {renderCircle(4)}
-            {renderCircle(5)}
-            {renderCircle(6)}
-            {renderCircle(7)}
-        </div>
+        <>
+            <Header player={currentPlayer} />
+            <div className="gameBoard" >
+                {initBoard()}
+            </div>
+            <Footer />
+        </>
     )
     
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = ({player}) => {
+  return (
+    <div className='panel header'>
+        <div className='header-text'>Player {player} Turn</div>
+    </div>
+  )
+}
+
+export default Header;
